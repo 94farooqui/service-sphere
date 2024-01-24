18af5ef0dfb91772668697eacdf73e9b987653cc
94farooqui <94.farooqui@gmail.com>
2024-01-24 18:57:09 +0400

    made changes in bug details card, sidebar, header and grid layout

--- src/components/BugDetailsCard.jsx
+++ src/components/BugDetailsCard.jsx
@@ -4,18 +4,22 @@ import BugStatusCard from "./BugStatusCard";
 import BugAuthor from "./BugAuthor";
 import BugAssigneeCard from "./BugAssigneeCard";
 import BugPropertiesCard from "./BugPropertiesCard";
+import BugConversations from "./BugConversations";
 
 const BugDetailsCard = ({ bug }) => {
   return (
     <div className="grid grid-cols-[auto_280px] gap-4">
       
-      <div className=" p-6 grid grid-cols-1 border-r border-slate-300 drop-shadow-sm">
+      <div className=" p-6 grid grid-cols-1 gap-4 border-r border-slate-300 drop-shadow-sm">
         <BugHeader bug={bug} />
-        
+        <div>
+            <h4 className="text-xl font-bold text-slate-700 mb-2">Conversations</h4>
+            <BugConversations />
+        </div>
         
       </div>
       
-      <div className="h-screen grid grid-cols-1 gap-2 py-4">
+      <div className="flex flex-col gap-2 p-4">
         <BugStatusCard/>
         <BugAuthor/>
         <BugAssigneeCard/>
--- src/components/BugHeader.jsx
+++ src/components/BugHeader.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 
 const BugHeader = ({bug}) => {
   return (
-    <div className='bg-white'>
+    <div className='bg-white p-8 rounded-lg grid grid-cols-1 gap-4'>
         <h4 className="text-xl font-semibold  text-slate-800">{bug.name}</h4>
-        <div className="flex justify-between">
-          <p>{bug.author}</p>
-          <p>{bug.dateCreated.toLocaleString()}</p>
-        </div>
-        <div className="flex justify-between">
-          <p>{bug.assignee}</p>
-          <p>{bug.status}</p>
+        <p>{bug.description}</p>
+        <div className='grid grid-cols-2 gap-4 mt-2'>
+            <div className="flex flex-col">
+                <h4 className='font-semibold text'>Project</h4>
+                <p>{bug.project}</p>
+            </div>
+            <div className="flex flex-col">
+                <h4 className='font-semibold text'>Requester</h4>
+                <p>{bug.author}</p>
+            </div>
+            <div className="flex flex-col">
+                <h4 className='font-semibold text'>Date</h4>
+                <p>{bug.dateCreated.toLocaleString()}</p>
+            </div>
+            <div className="flex flex-col">
+                <h4 className='font-semibold text'>Status</h4>
+                <p>{bug.status}</p>
+            </div>
         </div>
     </div>
   )
